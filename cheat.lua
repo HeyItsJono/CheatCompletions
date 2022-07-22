7e9b3248ecbc081ec7ede41f6de12b05fa71f65a
HeyItsJono <freymotifs@gmail.com>
2022-07-22 16:37:14 +1000

    Fix indentations

--- cheat.lua
+++ cheat.lua
@@ -2,13 +2,13 @@
 -- Run a subprocess of 'cheat -l', errors suppressed. This lists all available cheatsheets. Then use gsub to isolate the 'title' column and drop the headers row. Return list of cheatsheet names.
 local function get_cheats()
     local cheats = {}
-	local cheatlist = io.popen('2>nul cheat -l')
+    local cheatlist = io.popen('2>nul cheat -l')
     
     if cheatlist then
         for line in cheatlist:lines() do
-        	line = line:gsub(' .*',''):gsub('title:.*','')
+            line = line:gsub(' .*',''):gsub('title:.*','')
             if line ~= '' then
-            	table.insert(cheats, line)
+                table.insert(cheats, line)
             end
         end
         cheatlist:close()
@@ -22,17 +22,17 @@ clink.argmatcher("cheat")
 :addarg({get_cheats})
 :nofiles()
 :adddescriptions(
-	{"--init",									description = "Write a default config file to stdout"},
-	{"-a", "--all",								description = "Search among all cheatpaths"},
-	{"-c", "--colorize",						description = "Colorize output"},
-	{"-d", "--directories",						description = "List cheatsheet directories"},
-	{"-e", "--edit=",							description = "Edit <cheatsheet>"},
-	{"-l", "--list",							description = "List cheatsheets"},
-	{"-p", "--path=",        					description = "Return only sheets found on cheatpath <name>"},
-	{"-r", "--regex",              				description = "Treat search <phrase> as a regex"},
-	{"-s", "--search=",    						description = "Search cheatsheets for <phrase>"},
-	{"-t", "--tag=",          					description = "Return only sheets matching <tag>"},
-	{"-T", "--tags",               				description = "List all tags in use"},
-	{"-v", "--version",            				description = "Print the version number"},
-	{"--rm=",       							description = "Remove (delete) <cheatsheet>"}
+    {"--init",                                  description = "Write a default config file to stdout"},
+    {"-a", "--all",                             description = "Search among all cheatpaths"},
+    {"-c", "--colorize",                        description = "Colorize output"},
+    {"-d", "--directories",                     description = "List cheatsheet directories"},
+    {"-e", "--edit=",                           description = "Edit <cheatsheet>"},
+    {"-l", "--list",                            description = "List cheatsheets"},
+    {"-p", "--path=",                           description = "Return only sheets found on cheatpath <name>"},
+    {"-r", "--regex",                           description = "Treat search <phrase> as a regex"},
+    {"-s", "--search=",                         description = "Search cheatsheets for <phrase>"},
+    {"-t", "--tag=",                            description = "Return only sheets matching <tag>"},
+    {"-T", "--tags",                            description = "List all tags in use"},
+    {"-v", "--version",                         description = "Print the version number"},
+    {"--rm=",                                   description = "Remove (delete) <cheatsheet>"}
 )
